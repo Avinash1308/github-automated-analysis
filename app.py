4178585cacea1ba4cc52d886d774e35b6e8d2194
Avinash1308 <92975993+Avinash1308@users.noreply.github.com>
2023-06-24 12:02:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,87 @@
+from flask import Flask, render_template, request
+import requests
+import openai
+
+app = Flask(__name__)
+
+# Configure OpenAI API credentials
+openai.api_key = 'YOUR_OPENAI_API_KEY'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/analyze', methods=['POST'])
+def analyze():
+    # Get the GitHub user URL from the form input
+    user_url = request.form['github_url']
+    
+    # Fetch user repositories from GitHub API
+    repositories = fetch_user_repositories(user_url)
+    
+    if repositories:
+        most_complex_repository = find_most_complex_repository(repositories)
+        
+        if most_complex_repository:
+            complexity_score = evaluate_complexity(most_complex_repository)
+            analysis_justification = generate_analysis_justification(most_complex_repository)
+            
+            return render_template('result.html', repository=most_complex_repository, complexity_score=complexity_score, analysis_justification=analysis_justification)
+    
+    return render_template('result.html', repository=None)
+
+def fetch_user_repositories(user_url):
+    api_url = f"{user_url}/repos"
+    response = requests.get(api_url)
+    repositories = response.json()
+    return repositories
+
+def preprocess_code(repository):
+    # Implement code preprocessing techniques here
+    # Handle large repositories, Jupyter notebooks, package files, etc.
+    pass
+
+def evaluate_complexity(repository):
+    # Implement GPT-based evaluation of repository complexity here
+    # Use prompt engineering techniques and GPT APIs to assess complexity
+    prompt = "Evaluate the technical complexity of this code:\n"
+    code = repository['content']  # Assuming the repository structure contains 'content' field
+
+    # Concatenate the prompt and code for evaluation
+    evaluation_text = prompt + code
+
+    # Make an API call to OpenAI GPT to evaluate complexity
+    response = openai.Completion.create(
+        engine='text-davinci-003',
+        prompt=evaluation_text,
+        max_tokens=100,
+        n=1,
+        stop=None,
+        temperature=0.7
+    )
+
+    complexity_score = response.choices[0].text.strip()
+    return complexity_score
+
+def generate_analysis_justification(repository):
+    # Implement GPT-based analysis justification generation here
+    # Use the repository data and GPT APIs to generate analysis justification
+    pass
+
+def find_most_complex_repository(repositories):
+    most_complex_repo = None
+    max_complexity_score = float('-inf')
+
+    for repo in repositories:
+        preprocess_code(repo)
+        complexity_score = evaluate_complexity(repo)
+
+        if complexity_score > max_complexity_score:
+            max_complexity_score = complexity_score
+            most_complex_repo = repo
+
+    return most_complex_repo
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
